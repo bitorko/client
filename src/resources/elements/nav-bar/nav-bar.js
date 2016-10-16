3fd011d84615727315592f61c6c40319c683aacb
Sayem <afm.sayem@gmail.com>
2016-10-16 18:53:11 +0600

    Add navigation bar component

---
+++ src/resources/elements/nav-bar/nav-bar.js
@@ -0,0 +1,12 @@
+import {customElement, bindable, containerless} from 'aurelia-framework';
+// import {CssAnimator} from 'aurelia-animator-css';
+// import {inject} from 'aurelia-framework';
+
+// @customElement('nav-bar')
+// @inject(CssAnimator)
+export class NavBar {
+  @bindable router;
+  // constructor(animator) {
+  //   this.animator = animator;
+  // }
+}
